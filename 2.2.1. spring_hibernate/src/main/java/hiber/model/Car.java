0612c4d83652e313cf8fe8827a500b6cbe1db38b
Stepan <Stepanpan1201@gmail.com>
2024-04-13 15:58:09 +0300

    complete task

---
+++ 2.2.1. spring_hibernate/src/main/java/hiber/model/Car.java
@@ -0,0 +1,36 @@
+package hiber.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "cars")
+public class Car {
+    @Column(name = "model")
+    private String model;
+    @Id
+    private int series;
+
+    public Car() {
+    }
+
+    public Car(String model, int series) {
+        this.model = model;
+        this.series = series;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getSeries() {
+        return series;
+    }
+
+    public void setSeries(int series) {
+        this.series = series;
+    }
+}
